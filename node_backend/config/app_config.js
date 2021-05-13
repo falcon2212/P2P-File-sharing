d09201a82acbdb7d2fc234dd6db30424cf557f76
vemshari27 <vemshari27@gmail.com>
2021-05-13 20:08:57 +0530

    device based

--- node_backend/config/app_config.js
+++ node_backend/config/app_config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//     MONGO_HOST: "mongodb://khalid:khalid@window-drop-shard-00-00.fwtvj.mongodb.net:27017,window-drop-shard-00-01.fwtvj.mongodb.net:27017,window-drop-shard-00-02.fwtvj.mongodb.net:27017/mySecondDatabase?ssl=true&replicaSet=atlas-d051ku-shard-0&authSource=admin&retryWrites=true&w=majority",
-//     MONGO_DB: "",
-// }
-
 module.exports = {
-    MONGO_HOST: "mongodb://127.0.0.1:27017/",
-    MONGO_DB: "windrop_db",
+    MONGO_HOST: "mongodb://khalid:khalid@window-drop-shard-00-00.fwtvj.mongodb.net:27017,window-drop-shard-00-01.fwtvj.mongodb.net:27017,window-drop-shard-00-02.fwtvj.mongodb.net:27017/mySecondDatabase?ssl=true&replicaSet=atlas-d051ku-shard-0&authSource=admin&retryWrites=true&w=majority",
+    MONGO_DB: "",
 }
+
+// module.exports = {
+//     MONGO_HOST: "mongodb://127.0.0.1:27017/",
+//     MONGO_DB: "windrop_db",
+// }
 
