267bea5eb18ab7c9bc507ab771683854e9b318b6
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-05-15 17:16:41 +0530

    Added test for signup

--- node_backend/test/users.js
+++ node_backend/test/users.js
@@ -7,13 +7,22 @@ describe("users api", function(){
 
         var url = "https://backend-service-falcon2212.cloud.okteto.net/";
 
-        it("test1", function(){
+        it("returns status 200", function(){
             request(url, function(error, response, body){
-                //console.log(response)
+                //console.log(body)
                 expect(response.statusCode).to.equal(200);
+                //done();
             })
         })
+    })
 
+    describe("sign up", function(){
+        var url = "https://backend-service-falcon2212.cloud.okteto.net/users/add";
+
+        it("returns status 200", function(){
+            request.post(url).send({username:"Test", password:"Test", email:"Test@gmail.com", name: "Test"}).expect(200);
+            //done();
+        })
     })
 
 })
